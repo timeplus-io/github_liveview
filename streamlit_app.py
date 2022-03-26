c4806a650270f3e33f260525c0f3fb59b5c9c004
Jove Zhong <jove@timeplus.io>
2022-03-26 01:23:23 -0700

    change layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import json
 from rx import operators as ops
 
 import pandas as pd
+import altair as alt
 
 from timeplus import (
     Stream,
@@ -29,6 +30,8 @@ from timeplus import (
 
 from PIL import Image
 
+st.set_page_config(layout="wide")
+
 image = Image.open("detailed-analysis@2x.png")
 st.image(image, caption="Timeplus Real-time Insights for Github")
 #st.write("Fast + Powerful Real-Time Analytics Made Intuitive.")
@@ -42,55 +45,36 @@ env = (
 )
 Env.setCurrent(env)
 
-# show event count line chart
-with st.container():
-    sql="select window_end as time,count() as count from tumble(table(github_events),1h) group by window_end emit last 2d"
+col1, col2, col3 = st.columns([3,3,1])
+
+with col1:#st.container():
+    st.header('New events over time')
+    sql="select window_end as time,count() as count from tumble(table(github_events),10m) group by window_end emit last 2d"
     query = Query().sql(sql).create()
-    col = ['count']
+    col = [h["name"] for h in query.header()]
     def update_row0(row,name):
-        print(row[0])
-        data = row[1]
-        df = [data] #pd.DataFrame([data], columns=col)
+        data = {}
+        for i, f in enumerate(col):
+            data[f] = row[i]
+        df = pd.DataFrame([data], columns=col)
         if name not in st.session_state:
-            st.session_state[name] = st.line_chart(df)
+            c = alt.Chart(df).mark_line(point=alt.OverlayMarkDef(color="#D12D50")).encode(x='time:T',y='count:Q',tooltip=['time','count'],color=alt.value('#D53C97'))
+            st.session_state[name] = st.altair_chart(c, use_container_width=True)
         else:
             st.session_state[name].add_rows(df)
     stopper = Stopper()
-    query.get_result_stream(stopper).pipe(ops.take(6)).subscribe(
+    query.get_result_stream(stopper).subscribe(
         on_next=lambda i: update_row0(i,"linechart_cnt"),
         on_error=lambda e: print(f"error {e}"),
         on_completed=lambda: stopper.stop(),
     )
     query.cancel().delete()
 
-# show a changing single value for total events
-with st.empty():
-    #show the initial events first
-    sql="select count(*) from table(github_events) emit periodic 1s"
-    cnt=Query().execSQL(sql)["data"][0][0]
-    st.metric(label="Github events", value="{:,}".format(cnt))
-    st.session_state.last_cnt=cnt
-
-    #create a streaming query to update counts
-    sql=f"select {cnt}+count(*) as events from github_events"
-    query = Query().sql(sql).create()
-    def update_row(row):
-        delta=row[0]-st.session_state.last_cnt
-        if (delta>0):
-            st.metric(label="Github events", value="{:,}".format(row[0]), delta=row[0]-st.session_state.last_cnt)
-            st.session_state.last_cnt=row[0]
-    stopper = Stopper()
-    query.get_result_stream(stopper).pipe(ops.take(3)).subscribe(
-        on_next=lambda i: update_row(i),
-        on_error=lambda e: print(f"error {e}"),
-        on_completed=lambda: stopper.stop(),
-    )
-    query.cancel().delete()
-
-with st.container():
-    sql="""SELECT window_end as time,repo, group_array(distinct actor) AS watchers
+with col2:#st.container():
+    st.header('Hot repos')
+    sql="""SELECT window_end as time,repo, group_array(distinct actor) AS followers
 FROM hop(github_events,1m,10m) 
-WHERE type ='WatchEvent' GROUP BY window_end,repo HAVING length(watchers)>1 
+WHERE type ='WatchEvent' GROUP BY window_end,repo HAVING length(followers)>1 
 emit last 1h
 """
     query = Query().sql(sql).create()
@@ -111,3 +95,30 @@ emit last 1h
         on_completed=lambda: stopper.stop(),
     )
     query.cancel().delete()
+
+# show a changing single value for total events
+with col3:
+    st.header('Event count')
+    with st.empty():
+        #show the initial events first
+        sql="select count(*) from table(github_events) emit periodic 1s"
+        cnt=Query().execSQL(sql)["data"][0][0]
+        st.metric(label="Github events", value="{:,}".format(cnt))
+        st.session_state.last_cnt=cnt
+
+        #create a streaming query to update counts
+        sql=f"select {cnt}+count(*) as events from github_events"
+        query = Query().sql(sql).create()
+        def update_row(row):
+            delta=row[0]-st.session_state.last_cnt
+            if (delta>0):
+                st.metric(label="Github events", value="{:,}".format(row[0]), delta=row[0]-st.session_state.last_cnt)
+                st.session_state.last_cnt=row[0]
+        stopper = Stopper()
+        query.get_result_stream(stopper).subscribe(
+            on_next=lambda i: update_row(i),
+            on_error=lambda e: print(f"error {e}"),
+            on_completed=lambda: stopper.stop(),
+        )
+        #query.cancel().delete()
+
